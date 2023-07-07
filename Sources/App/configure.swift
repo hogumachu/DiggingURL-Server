38d410d9aaa22549486883b68a73072068eb5916
hogumachu <castle_jun@daum.net>
2023-07-07 16:52:36 +0900

    feat: add database configuration

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,10 +1,30 @@
 import Vapor
+import Fluent
+import FluentMySQLDriver
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    // register routes
+    
+    // MARK: - Configure Database
+    
+    try configureDatabase(using: app)
+    
+    // MARK: - Register Routes
+    
     try CompositionRoute.routes(app)
+}
+
+
+private func configureDatabase(using app: Application) throws {
+    let tlsConfiguration: TLSConfiguration = .forClient(certificateVerification: .none)
+    let databaseConfig: DatabaseConfigurationFactory = .mysql(
+        hostname: "localhost",
+        username: "root",
+        password: "",
+        database: "diggingurl",
+        tlsConfiguration: tlsConfiguration
+    )
+    app.databases.use(databaseConfig, as: .mysql)
 }
